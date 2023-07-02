1a849edb8eaf242144a4ddf9ff1cf83992389438
Anderson <anderson21fonseca@gamil.com>
2023-07-02 16:14:22 -0300

    Criado uma tela de login e um layout de login

--- app/src/main/java/com/operacao/bdandroid/MainActivity.java
+++ app/src/main/java/com/operacao/bdandroid/MainActivity.java
@@ -1,13 +1,18 @@
 package com.operacao.bdandroid;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.content.pm.LabeledIntent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,9 +34,21 @@ public class MainActivity extends AppCompatActivity {
     public void insertDados(View view){
         String nome=EDITAR.getText().toString();
       //  Usuario user = new Usuario(0,"Anderson",29,"84991958497");
-        MYbD.usuarioDao().inserir(new Usuario(0,"Andinho",29,"84"));
+        MYbD.usuarioDao().inserir(new Usuario(0,""+nome,29,"84"));
         Toast.makeText(this, "Dados salvos com sucesso ", Toast.LENGTH_SHORT).show();
+        listaDados();
         MYbD.close();
     }
 
+    public void listaDados(){
+        String dados="";
+
+        for(Usuario usuario:MYbD.usuarioDao().all()){
+            dados=" informação id "+usuario.getId()+" nome "+usuario.getNome()+" idade "+usuario.getIdade();
+        }
+        AlertDialog.Builder alA = new AlertDialog.Builder(this);
+        alA.setMessage(" "+dados);
+        alA.show();
+
+    }
 }
